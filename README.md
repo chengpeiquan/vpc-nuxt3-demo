e01062d0384e0ae9aeb659fdefe4af9e1202538a
chengpeiquan <chengpeiquan@chengpeiquan.com>
2022-12-16 23:31:06 +0800

    chore: deploy

--- README.md
+++
@@ -1,3 +0,0 @@
-# vpc-nux3-demo
-
-Example of using [vue-picture-cropper](https://github.com/chengpeiquan/vue-picture-cropper) in Nuxt 3.
--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  telemetry: false,
-  app: {
-    baseURL: '/vpc-nux3-demo/',
-  },
-})
